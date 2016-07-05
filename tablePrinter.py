fb062b4327b5c202be38cd43d7954f0e4389d526
Anthony Gladman <anthony.gladman@gmail.com>
2016-07-05 17:08:23 +0100

    working but I'm not happy with it

--- tablePrinter.py
+++ tablePrinter.py
@@ -1,5 +1,8 @@
 # tablePrinter.py - an exercise from p143 of ATBS
 
+# script returns the expected result, but to be honest it looks like
+# a horrible mess. I bet this is not the best way to do it.
+
 tableData = [['apples', 'oranges', 'cherries', 'banana'],
              ['Alice', 'Bob', 'Carol', 'David'],
              ['dogs', 'cats', 'moose', 'goose']]
@@ -8,13 +11,23 @@ def printTable(tableData):
     colWidths = [0] * len(tableData)
 
     # capture the colWidths to use later in .rjust()
-    # correct result should be [8, 5, 5]
     for i in range(len(tableData)):
-        colWidths[i] = (max(tableData[i], key=len))
+        colWidths[i] = len((max(tableData[i], key=len)))
 
+    # print the table
 
-    return colWidths # remove this line and lines 18 and 20 once this part is working
+    # attempt without using range - failed
+    # a = 0
+    # for word in tableData[a]:
+    #     for wordlist in tableData:
+    #         print(word)
+    #         a += 1
+    # print('\n')
 
-debug = printTable(tableData)
+    # from p103.py - didn't understand it then either and had to google
+    for j in range(len(tableData[0])):
+        for i in range(len(tableData)):
+            print(tableData[i][j].rjust(colWidths[i]), end=' ')
+        print('\n')
 
-print(debug)
+printTable(tableData)
