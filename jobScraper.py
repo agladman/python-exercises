9528811682ce13fa111aefb49ecc5c9d9e273aa4
Anthony Gladman <anthony.gladman@gmail.com>
2016-07-08 18:03:03 +0100

    sort of working now, going to continue on a branch

--- jobScraper.py
+++ jobScraper.py
@@ -3,7 +3,7 @@
 # Next step is to find names of individuals holding those jobs.
 
 import os, logging, re, pprint, datetime
-logging.basicConfig(filename='../../sgmTidy-log-{::%Y%m%d-%H%M}.txt'.format(datetime.datetime.now()), level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename='../../jobScraper-{:%Y%m%d-%H%M}-log.txt'.format(datetime.datetime.now()), level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
 logging.debug('Start of program')
 
 while True:
@@ -19,31 +19,37 @@ while True:
         print('Something went wrong: {}'.format(str(e)))
         logging.critical('Exception: {}'.format(str(e)))
 
+all_lines = datafile.readlines()
+
 jobtitles = {}
 jtregex = re.compile(r'<job>(.*?)</job>')
 
-for i, line in enumerate(datafile):
+for i, line in enumerate(all_lines):
     result = jtregex.findall(line)
     for item in result:
         if item not in jobtitles:
             jobtitles.setdefault(item, 0)
         jobtitles[item] += 1
-    logging.debug('jtregex run on line {0}'.format(i + 1)
+    # logging.debug('jtregex run on line {0}'.format(i + 1))
 
 # TODO: capture names matching each job titles
 
 jtitles = list(jobtitles.keys()) # so that order becomes fixed
+logging.debug('jtitles: {}'.format(jtitles))
 jholders = []
 
-for j, line in enumerate(datafile):
+for j, line in enumerate(all_lines):
     for k in jtitles:
         jhregex = re.compile(r'<job>{}</job><name>(.*?)</name>'.format(re.escape(k)))
-        logging.debug('jhregex set to: {}'.format(jhregex))
+        if j <= 1: # stops log from getting too big
+            logging.debug('jhregex set to: {}'.format(jhregex))
         result = jhregex.findall(line)
-        logging.debug('result: {}'.format(result))
+        if j <= 1: # stops log from getting too big
+            logging.debug('result: {}'.format(result))
         jholders += result
     logging.debug('jhregex loop on line {0}'.format(j + 1))
 
+logging.debug('jholders: {}'.format(jholders))
 holders = {zip(jtitles, jholders)}
 
 # end TODO for capturing names
