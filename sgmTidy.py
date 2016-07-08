bad710d5ef4a1f132968db5fdda4859c9cc91a76
Anthony Gladman <anthony.gladman@gmail.com>
2016-07-08 13:05:28 +0100

    runs without errors but doesn't achieve desired result

--- sgmTidy.py
+++ sgmTidy.py
@@ -1,27 +1,44 @@
 #!/usr/bin/env python3
 # sgmTidy.py cleans up raw sgml files ready for further analysis
 
-# raising Exception and I don't know why yet
+import logging, re, os
 
-import re
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+logging.debug('Start of program')
 
-def get_input_file():
-    while True:
-        try:
-            input_file = input('Enter full path to input file: ')
-            if os.path.isfile(input_file):
-                return input_file
-                break
-            else:
-                print('Gah! Check file exists in that location and try again.')
-        except Exception as e:
-            print('Something went wrong.')
+def multiple_replace(dict, text):
+    # from http://stackoverflow.com/a/15175239/6409460
+    # Create a regular expression  from the dictionary keys
+    logging.debug('creating regex')
+    regex = re.compile('({})'.format('|'.join(map(re.escape, dict.keys()))))
 
-with open(get_input_file()) as input_file:
+    # For each match, look-up corresponding value in dictionary
+    logging.debug('looking up match in dictionary')
+    return regex.sub(lambda mo: dict[mo.string[mo.start():mo.end()]], text)
 
-    re.sub(r'>\s<', r'><', input_file)
-    re.sub(r'\n', '', input_file)
-    re.sub(r'(<[second|prim]ary)', r'\n\1', input_file)
+patterns = {'>\s<': '><', '\n': '', '(<[second|prim]ary)': '\n\1'}
 
-close(input_file)
-print('Script sgmTidy.py complete')
+if __name__ == '__main__':
+
+    def get_file_path():
+        while True:
+            try:
+                file_path = input('Enter filepath: ')
+                if os.path.isfile(file_path):
+                    return file_path
+                    break
+                else:
+                    print('Check file exists in that location and try again.')
+            except Exception as e:
+                print('Something went wrong: {}'.format(str(e)))
+
+    with open(get_file_path(), 'r+') as datafile:
+        logging.debug('opening datafile')
+        output = multiple_replace(patterns, datafile.read())
+        logging.debug('fetching output')
+        datafile.write(output)
+        logging.debug('writing output')
+
+    datafile.close()
+    print('Script sgmTidy.py complete')
+    logging.debug('End of program')
