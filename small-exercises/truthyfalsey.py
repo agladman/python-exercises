1ac71334fce54999389324bb8d28bd7be879d4f3
Anthony Gladman <anthony.gladman@gmail.com>
2017-01-02 10:19:16 +0000

    docstrings

--- small-exercises/truthyfalsey.py
+++ small-exercises/truthyfalsey.py
@@ -2,40 +2,25 @@
 """Like Knifey-Spooney, only more Boolean."""
 
 def truthyfalsey(i, v):
+    """Evaluates empty variable v against different comparisons to True and False."""
     elist = "None True False '' [] () 0 0.0".split()
-    print(f'{i + 1}: x is {elist[i]}. If triggers: ', end='')
+    print(f'{i + 1}: x is {elist[i]}; if triggers: ', end='')
     iflist = []
     if v:
-        # print('if x', end='')
-        # iflist.append('if x')
         iflist.append('A')
     if not v:
-        # print('if not x', end='')
-        # iflist.append('if not x')
         iflist.append('B')
     if v == True:
-        # print('if x == True', end='')
-        # iflist.append('if x == True')
         iflist.append('C')
     if v == False:
-        # print('if x == False', end='')
-        # iflist.append('if x == False')
         iflist.append('D')
     if v is True:
-        # print('if x is True', end='')
-        # iflist.append('if x is True')
         iflist.append('E')
     if v is False:
-        # print('if x is False', end='')
-        # iflist.append('if x is False')
         iflist.append('F')
     if v is not False:
-        # print('if x is not False', end='')
-        # iflist.append('if x is not False')
         iflist.append('G')
     if v is not True:
-        # print('if x is not True', end='')
-        # iflist.append('if x is not True')
         iflist.append('H')
     ifstring = ', '.join([x for x in iflist])
     print(f'{ifstring}; ', end = '')
