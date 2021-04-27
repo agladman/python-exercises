fe8275cf3b948ce72ebce2f42dc8a10a4f935a4e
agladman <20296658+agladman@users.noreply.github.com>
2021-04-27 10:36:44 +0100

    adding recursive solution

--- small-exercises/adder.py
+++ small-exercises/adder.py
@@ -1,12 +1,22 @@
+#!/usr/bin/env python3
 # Write a program that asks the user for a number n and prints the sum of the numbers 1 to n
 
-print('Enter a number: ')
-num = int(input())
-sum = 0
+def sum1(num):
+    "a first attempt from long ago..."
+    sum = 0
+    for i in range(0, num):
+        sum = sum + i
+    sum = sum + num
+    return sum
 
-for i in range(0, num):
-    sum = sum + i
+def sum2(num):
+    "recursive attempt"
+    if num == 0:
+        return 0
+    else:
+        return num + sum2(num -1)
 
-sum = sum + num
-
-print(sum)
+if __name__ == '__main__':
+    num = int(input('Enter a number: '))
+    print(f'sum1: {sum1(num)}')
+    print(f'sum2: {sum2(num)}')
