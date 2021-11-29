c1c85c9c6fae1c1541d4af3c29c6ddff302dd13a
agladman <20296658+agladman@users.noreply.github.com>
2021-11-29 17:58:22 +0000

    Updated to use Python 3.10 match case in mpace()

--- small-exercises/readtime.py
+++ small-exercises/readtime.py
@@ -7,20 +7,41 @@ Calculates reading time based on average pace of 130 words per minute.
 
 import sys, time
 
-# slow = 100
-norm = 130
-# fast = 160
-
-def calc(w, pace=norm):
-    return w/pace * 60
+def mpace(p):
+    if type(p) == int and p > 0:
+        return p
+    else:
+        match p:
+            case "slow":
+                return 100
+            case "average":
+                return 130
+            case "fast":
+                return 160
+            case _:
+                raise ValueError
 
 def main():
-    w = None
-    if len(sys.argv) >= 2:
-        w = int(sys.argv[1])
-    if w is None:
-        w = int(input("Enter wordcount: "))
-    sec = calc(w)
+
+    words = None
+    pace = 130
+
+    # capture words and pace from sys args if passed
+    if len(sys.argv) == 2:
+        words = int(sys.argv[1])
+    elif len(sys.argv) > 2:
+        words = int(sys.argv[1])
+        p = sys.argv[2]
+        if p.isnumeric():
+            p = int(p)
+        pace = mpace(p)
+
+    # get words from user if still needed, pace stays as default
+    if words is None:
+        words = int(input("Enter wordcount: "))
+
+    # perform the calculation
+    sec = words/pace * 60
     ty_res = time.gmtime(sec)
     res = time.strftime("%Hh:%Mm:%Ss", ty_res)
     print(f"Reading time: {res}")
